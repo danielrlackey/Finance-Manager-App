0918126115a4934b61d3678c3b34fc38a6c52429
danielrlackey <65509781+danielrlackey@users.noreply.github.com>
2020-08-19 14:49:05 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses.js";
-import { setTextFilter } from "./actions/filters.js";
-import getVisibleExpenses from "./selectors/expenses.js";
 import "normalize.css/normalize.css";
 import "./styles/styles.css";
 
 const store = configureStore();
 
-
+console.log("testing")
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
-)
+);
 
 ReactDOM.render(jsx, document.getElementById("app"));
