6202028f0a15d0ebde438e5a24b99d356a9a8d73
danielrlackey <65509781+danielrlackey@users.noreply.github.com>
2020-08-25 19:03:55 -0400

    Add files via upload

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,5 +14,7 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig)
 
   firebase.database().ref().set({
-      name: "daniel lackey"
+      name: "daniel lackey",
+      age: "30",
+    
   })
